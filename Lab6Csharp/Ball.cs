c2a99f27dac98fa7e75d16425903dc004eb20ab2
UnnamedH <55529629+UnnamedH@users.noreply.github.com>
2020-09-09 21:23:52 +0300

    Добавьте файлы проекта.

---
+++ Lab6Csharp/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6Csharp
+{
+    class Ball
+    {
+        int x, y;
+        private int currentX = 100, currentY = 100;
+        private static object locker = new object();
+        private Random random = new Random();
+
+        public Ball()
+        {
+            x = random.Next(30, 150);
+            y = random.Next(30, 150);
+        }
+
+        public int[] moveBall()
+        {
+            int maxX = 400, maxY = 400;
+            currentX = x + currentX;
+            currentY = y + currentY;
+            int[] nums = { currentX, currentY };
+            return nums;
+        }
+
+        public void drawCircle(Form menu, Graphics grap)
+        {
+            lock (locker)
+            {
+                int[] resCoordinates;
+                do {
+                    resCoordinates = moveBall();
+                    Pen blackPen = new Pen(Color.Black, 3);
+                    Pen whitePen = new Pen(menu.BackColor, 3);
+                    SolidBrush blackBrush = new SolidBrush(Color.Black);
+                    SolidBrush whiteBrush = new SolidBrush(menu.BackColor);
+                    Rectangle rect = new Rectangle(resCoordinates[0], resCoordinates[1], 30, 30);
+                    grap.DrawEllipse(blackPen, rect);
+                    grap.FillEllipse(blackBrush, rect);
+                    Thread.Sleep(300);
+                    grap.DrawEllipse(whitePen, rect);
+                    grap.FillEllipse(whiteBrush, rect);
+                }
+                while 
+                (resCoordinates[0] < 500 && resCoordinates[0] > 0 && resCoordinates[0] < 500 && resCoordinates[0] > 0);
+            }
+        }
+
+    }
+}
---
+++ Lab6Csharp/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6Csharp
+{
+    public partial class Form1 : Form
+    {
+        static object locker = new object();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void prior(Thread obj, string status)
+        {
+            switch (status)
+            {
+                //Lowest, BelowNormal, Normal, AboveNormal, Highest
+                case "Lowest":
+                    obj.Priority = ThreadPriority.Lowest;
+                    break;
+                case "BelowNormal":
+                    obj.Priority = ThreadPriority.BelowNormal;
+                    break;
+                case "Normal":
+                    obj.Priority = ThreadPriority.Normal;
+                    break;
+                case "AboveNormal":
+                    obj.Priority = ThreadPriority.AboveNormal;
+                    break;
+                case "Highest":
+                    obj.Priority = ThreadPriority.Highest;
+                    break;
+                default:
+                    obj.Priority = ThreadPriority.Normal;
+                    break;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics grap = CreateGraphics();
+            List<Ball> listBalls = new List<Ball>() { };
+            List<Thread> threads = new List<Thread>() { };
+            int numberBalls = 2;
+            for (int i = 0; i < numberBalls; i++)
+            {
+                listBalls.Add(new Ball());
+                threads.Add(new Thread(delegate ()
+                {
+                        listBalls[i-1].drawCircle(this, grap);
+                }));
+                threads[i].Start();
+            }
+
+                prior(threads[0], "Lowest");
+
+            }
+    }
+}
